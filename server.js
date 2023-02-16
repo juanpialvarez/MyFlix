21501ea81267805755079588299617a14698bdd0
juanpialvarez <juanpabloalvarez@gmail.com>
2023-02-16 15:20:27 -0600

    Fix bug documentation page

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ http
       }
     );
     if (q.pathname.includes("documentation")) {
-      filePath = __dirname + "documentation.html";
+      filePath = __dirname + "\\documentation.html";
     } else {
       filePath = "index.html";
     }
