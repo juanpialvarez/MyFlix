9d89aa0a8f3ae66289582d646076b0c0b78fdab7
juanpialvarez <juanpabloalvarez@gmail.com>
2023-02-28 16:32:41 -0600

    Fix env variable

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const movies = Models.Movie;
 const users = Models.User;
 
 // mongoose.connect("mongodb://127.0.0.1/myFlix", { useNewUrlParser: true });
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.CONNECTION_URI);
 mongoose.set("strictQuery", true);
 const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
