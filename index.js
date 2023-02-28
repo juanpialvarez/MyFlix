34d0af9a1d18f25134b6a938f4fc004b22f649c9
juanpialvarez <juanpabloalvarez@gmail.com>
2023-02-28 16:49:43 -0600

    Fix bug in environment variable

--- index.js
+++ index.js
@@ -65,28 +65,19 @@ let auth = require("./auth")(app);
 
 // Get movies
 
-app.get("/movies", (req, res) => {
-  movies
-    .find()
-    .then((movie) => res.status(200).json(movie))
-    .catch((err) => {
-      console.err(err);
-      res.status(500).send(`Error: ${err}`);
-    });
-});
-// app.get(
-//   "/movies",
-//   passport.authenticate("jwt", { session: false }),
-//   (req, res) => {
-//     movies
-//       .find()
-//       .then((movie) => res.status(200).json(movie))
-//       .catch((err) => {
-//         console.err(err);
-//         res.status(500).send(`Error: ${err}`);
-//       });
-//   }
-// );
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    movies
+      .find()
+      .then((movie) => res.status(200).json(movie))
+      .catch((err) => {
+        console.err(err);
+        res.status(500).send(`Error: ${err}`);
+      });
+  }
+);
 
 // Get movie by title
 app.get(
