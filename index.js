644d9d42fbe0a1e2afa99a6f1e561b5c5ecdc2a1
juanpialvarez <juanpabloalvarez@gmail.com>
2023-03-02 12:46:13 -0600

    fix bug

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ require("./passport");
 const movies = Models.Movie;
 const users = Models.User;
 
-mongoose.connect("mongodb://127.0.0.1/myFlix", { useNewUrlParser: true });
-// mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true });
+// mongoose.connect("mongodb://127.0.0.1/myFlix", { useNewUrlParser: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true });
 mongoose.set("strictQuery", true);
 const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
